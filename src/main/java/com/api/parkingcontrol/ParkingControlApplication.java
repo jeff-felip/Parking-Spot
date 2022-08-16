b0c4ac5026e02eba167fe5fbb31aa1e9ea8a1820
jefferson <jeff.keiryw@gmail.com>
2022-08-16 17:17:44 -0300

    acrescentando spring security ao projeto

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -2,6 +2,7 @@ package com.api.parkingcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -10,6 +11,7 @@ public class ParkingControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("keiryw123"));
 	}
 
 }
